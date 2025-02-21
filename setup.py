20c1cc6ba1a276ec42c3bd7a56dcf73f013a7d79
Winfried Koehler <78253700+wirbel-at-vdr-portal@users.noreply.github.com>
2025-02-21 08:44:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name="pysmithplot",
-      version="0.2.0",
+      version="0.2.1",
       packages=["smithplot"],
       description="An extension for Matplotlib providing a projection class to generate high quality Smith Chart plots.",
       long_description=read('README.md'),
